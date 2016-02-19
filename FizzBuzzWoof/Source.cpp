7bf9b604a7d0413ddb8862059f14124682480433
JoashNaidoo <joashnaidoo19111994@gmail.com>
2016-02-20 00:07:49 +0200

    FizzBuzzWoof

--- FizzBuzzWoof/Source.cpp
+++ FizzBuzzWoof/Source.cpp
@@ -5,17 +5,20 @@ int main()
 {
 	int i;
 
-	for (i = 1; i <= 20; i++)
+	for (i = 1; i <= 50; i++)
 	{
 
-		if (i % 3 == 0 && i % 5 == 0)
-			cout << "FizzBuzz" << endl;
+		if (i % 3 == 0 && i % 5 == 0 && i % 7 == 0)
+			cout << "FizzBuzzWoof" << endl;
 
 		else if (i % 3 == 0)
 			cout << "Fizz" << endl;
 
 		else if (i % 5 == 0)
 			cout << "Buzz" << endl;
+		
+		else if (i % 7 == 0)
+			cout << "Woof" << endl;
 
 		else
 			cout << i << endl;
